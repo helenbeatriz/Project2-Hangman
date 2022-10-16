e7cd6083e4a8e19e9c8fa32097d7bc583ac16946
Helen Cantu <helen_beatriz02@outlook.com>
2022-10-16 19:06:02 +0000

    ADDED: WORDS TO BE ABLE TO USE IN MY FUNCTION FOR HANGMAN

--- javascript.js
+++ javascript.js
@@ -8,3 +8,36 @@ else {
     buttonrule.style.display = "none"
 }
 }
+//WORDS 
+const words = [
+    word001 = {
+        name: "IRELAND"
+    },
+    word002 = {
+        name:"TEMPTATION"
+    },
+    word003 = {
+        name:"SCUBA"
+    },
+    word004 = {
+        name: "DESTRUCTION"
+    },
+    word005 = {
+        name: "DENOTATION"
+    },
+    word006 = {
+        name: "PILLOW"
+    },
+    word007 = { 
+        name: "MASSIVE"
+    },
+    word008 = {
+        name: "LAZINESS"
+    },
+    word009 = {
+        name:"HYPOCHONDRIA"
+    },
+    word010 = {
+        name:"PSYCHOPATH"
+    }
+]
