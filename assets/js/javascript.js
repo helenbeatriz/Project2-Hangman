0ce46471a6e51614a930e96dd117a8135bd2798b
Helen Cantu <helen_beatriz02@outlook.com>
2022-10-19 12:25:27 +0000

    Editing rule to create a random words and editing media queries for keyboard/ styling the page

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,69 +1,54 @@
 function rule() {
     var buttonrule = document.getElementById('rulestext');
+buttonrule.innerHTML = "Rules are going to be here I still need to type them"
+// if (buttonrule.style.display === "block") {
+//     buttonrule.style.display= "none";
+// }
+// else {
+//     buttonrule.style.display = "none"
+// }
 
-if (buttonrule.style.display === "none") {
-    buttonrule.style.display= "block";
 }
-else {
-    buttonrule.style.display = "none"
-}
-console.log(rule)
-}
-//WORDS 
-let sortedSecretWord;
-let wordsList = [];
-const words = [
-    word001 = {
-        name: "IRELAND"
-    },
-    word002 = {
-        name:"TEMPTATION"
-    },
-    word003 = {
-        name:"SCUBA"
-    },
-    word004 = {
-        name: "DESTRUCTION"
-    },
-    word005 = {
-        name: "DENOTATION"
-    },
-    word006 = {
-        name: "PILLOW"
-    },
-    word007 = { 
-        name: "MASSIVE"
-    },
-    word008 = {
-        name: "LAZINESS"
-    },
-    word009 = {
-        name:"HYPOCHONDRIA"
-    },
-    word010 = {
-        name:"PSYCHOPATH"
-    }
-];
+// Game starts here 
+var words = [
+	"destruction",
+	"javascript",
+	"laziness",
+	"massive",
+	"pillow",
+	"psycopath",
+	"strange",
+	"happiness",
+	"challenge",
+	"organic",
+	"happy",
+	"stress",
+	"hypochondria",
+	"denonation"]
 
+let answer = '';
+let maxWrong = 6;
+let mistakes = 0;
+let guessed = [];
+// let wordStatus = null;
+// Functions executed - > 
+randomWord();
+generateButtons();
 
-createSecretWord();
-function createSecretWord(){
-    const indexWord = parseInt(Math.random() * words.length)
-    
-    sortedSecretWord =  words[indexWord].name;
-    console.log(sortedSecretWord);
-}
-wordOnScreen();
-function wordOnScreen(){
-
-    const secretWord = document.getElementById("secretword");
-    console.log(secretWord)
-    secretWord.innerHTML = "";
-
-    for(i=0; i < sortedSecretWord.length; i++) {
-    if (wordsList[i] == undefined){
-        wordsList[i] = "&nbsp;"
-          wordOnScreen.innerHTML = wordOnScreen.innerHTML + wordsList[i] 
-        }
-    }
-}
+function randomWord() {
+    answer = words[Math.floor(Math.random() * words.length)];
+  }
+  function generateButtons() {
+    let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+      `
+        <button
+          class="btn btn-lg btn-primary m-2"
+          id='` + letter + `'
+          onClick="handleGuess('` + letter + `')"
+        >
+          ` + letter + `
+        </button>
+      `).join('');
+  
+    document.getElementById('keyboard').innerHTML = buttonsHTML;
+  }
